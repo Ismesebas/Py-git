c094e43aa9948d1e7251576d729a35dc8875fc21
Sebastian Fuentes <sebastianfuentes53@gmail.com>
2023-04-27 15:19:57 -0600

    subir clase 1

---
+++ clase1.py
@@ -0,0 +1,18 @@
+print("Saludos desde la clase 1")
+("Saludos desde la clase 1")
+print ("el operador de suma es: +")
+"el operador de suma es: +"
+print("5+2=",5+2)
+print("el operador de resta es: -")
+#el operador de resta es: -
+print( "10-20=",10-20)
+print("el operador de division es: /")
+#el operador de division es: /
+print("5*2="), 5*2
+
+print ("el operador de division es; /")
+#el operador de division es; /
+print("5/3=", 5/3)
+print("8//3=", 8/3)
+print("8//3=", 8//3)
+round(2.66)
