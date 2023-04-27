ab5a856c0805648b4f715f32aa9a40ce6062c57d
Sebastian Fuentes <sebastianfuentes53@gmail.com>
2023-04-27 15:58:46 -0600

    practica 1 27-4-2023

---
+++ practica1.py
@@ -0,0 +1,4 @@
+print("Esta es la primera practica"), print("probando codigo")
+print("el", "uso", "de", "las", "comas")
+print("8+3"), (8+3), print("hola \ncuco")
+print("saludos \ndesde \npython\n\n\n\n\n\n\n")
