e05854472d5375409a5cc989718831d8525a7778
Sebastian Fuentes <sebastianfuentes53@gmail.com>
2023-05-03 14:35:22 -0600

    practica miercoles

---
+++ calcula.py
@@ -0,0 +1,9 @@
+#Calculadora
+
+numero1= int(input( "Ingrese su primer numero "))
+numero2= int(input("Ingrese su segundo numero "))
+
+
+resultado= numero1 + numero2
+print("La suma es:", resultado)
+
---
+++ variable.py
@@ -0,0 +1,10 @@
+#uso de round
+numero= 3.141516123456
+digitos=3
+
+print(round((10.589-1.987),digitos))
+print(round(1.578954,2))
+
+
+print(0.5000000)
+print(0.5501)
